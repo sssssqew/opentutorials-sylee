a03c78304d0b60f95d9c96cc1f949470ee519957
sylee <sssssqew1235@gmail.com>
2014-11-20 22:02:25 +0900

    added the science

--- array.php
+++ array.php
@@ -1,5 +1,5 @@
 <?php
-$grades = array("korean"=> 78,"math"=>95,"eng"=>100);
+$grades = array("korean"=> 78,"math"=>95,"eng"=>100,"science"=>83);
 foreach($grades as $key => $value){
 	echo "$key , $value<br />";
 }
